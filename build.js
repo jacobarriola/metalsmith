8ea86a61a5c265e84bf3d538f6e2735002953fb2
Jacob Arriola <jacob.arriola@gmail.com>
2015-11-19 23:09:22 -0800

    Add posts and livereload

--- build.js
+++ build.js
@@ -1,18 +1,38 @@
 var metalsmith  = require('metalsmith'),
     markdown    = require('metalsmith-markdown'),
-    inplace    = require('metalsmith-in-place'),
-    layouts     = require('metalsmith-layouts');
+    layouts     = require('metalsmith-layouts'),
+    collections = require('metalsmith-collections'),
+    permalinks  = require('metalsmith-permalinks'),
+    serve       = require('metalsmith-serve'),
+    watch       = require('metalsmith-watch');
 
 /**
  * Build
  */
 
 metalsmith(__dirname)
+  .metadata({
+    site: {
+      title: 'Jacob Arriola'
+    }
+  })
   .use(markdown())
   .use(layouts({
     engine:'handlebars',
     partials: 'partials'
   }))
+  .use(serve({
+    port: 8080,
+    verbose: true
+  }))
+  .use(watch({
+    pattern: '**/*',
+    livereload: true
+  }))
   .build(function(err) {
-    if (err) throw err;
+    if (err) {
+      throw err;
+    } else {
+      console.log('✔︎ Build complete, yo!');
+    }
   });
